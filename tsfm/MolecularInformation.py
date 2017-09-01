1345599ccd8eeb85750984c3d02ed998c4d61016
Travis Lawrence <tlawrence3@ucmerced.edu>
2017-09-01 14:00:16 -0700

    testing class method docstring formating

--- tsfm/MolecularInformation.py
+++ tsfm/MolecularInformation.py
@@ -59,6 +59,12 @@ class DistanceCalculator:
     def get_distance(self, ResultsDict):
         """
         Requires a dict[str, :class:`FunctionLogoResults`] and returns a pairwise distance matrix using the distance metric indicated during instantiation. 
+
+        Args:
+            ResultsDict dict[str, :class:`FunctionLogoResults`]: ``ResultsDict`` 
+                is a dictionary of dataset labels as keys and :class: 
+                `FunctionLogoResults` as values. 
+
         """
         for result in ResultsDict:
             for coord in ResultsDict[result].basepairs:
