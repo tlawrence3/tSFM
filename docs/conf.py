c35a27a7652d28a3cfe12f144b560531fa04c27a
Travis Lawrence <tlawrence3@ucmerced.edu>
2017-09-01 15:03:39 -0700

    added source code link to docs

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
               'sphinx.ext.napoleon',
-              'sphinx.ext.intersphinx']
+              'sphinx.ext.intersphinx', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
