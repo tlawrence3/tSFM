fe4176f776987e7bee814e55fcb5c1874dc2150d
Travis Lawrence <tlawrence3@ucmerced.edu>
2017-08-31 22:18:09 -0700

    updating docs

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['tsfm.exact', 'statsmodels', 'argparse', 'numpy', 'pandas', 'mpmath', 'patsy',
+MOCK_MODULES = ['exact', 'statsmodels', 'argparse', 'numpy', 'pandas', 'mpmath', 'patsy',
                 'scipy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
