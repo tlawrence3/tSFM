0ddc65ee7e1943362b0076b098a3807d22918591
Travis Lawrence <tlawrence3@ucmerced.edu>
2017-09-06 15:22:00 -0700

    testing latex math equations again

--- tsfm/MolecularInformation.py
+++ tsfm/MolecularInformation.py
@@ -209,7 +209,7 @@ class DistanceCalculator:
 
         .. math::  
             
-            D(X,Y) \equiv
+            D(X,Y) \equiv \sum_{f \in F}
 
         """
         step = self.entropy(pi1*dist1+pi2*dist2) - (pi1*self.entropy(dist1) + pi2*self.entropy(dist2))
