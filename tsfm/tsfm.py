d66df462854de281e3ea7a7f34a358ad5b76a127
Travis Lawrence <tlawrence3@ucmerced.edu>
2020-05-19 11:41:41 -0400

    Updated FunctionLogo init calls within tsfm.py

--- tsfm/tsfm.py
+++ tsfm/tsfm.py
@@ -94,19 +94,19 @@ def main():
     if (args.text):
         for prefix in args.file_prefix:
             prefix_name = prefix.split("/")[-1]
-            logo_dict[prefix_name] = MolecularInformation.FunctionLogo(args.text, "text", args.nosingle)
+            logo_dict[prefix_name] = MolecularInformation.FunctionLogo(args.text, "text", None, args.nosingle)
     elif (args.cove):
         for prefix in args.file_prefix:
             prefix_name = prefix.split("/")[-1]
-            logo_dict[prefix_name] = MolecularInformation.FunctionLogo(args.cove, "cove", args.nosingle)
+            logo_dict[prefix_name] = MolecularInformation.FunctionLogo(args.cove, "cove", None, args.nosingle)
     elif (args.infernal):
         for prefix in args.file_prefix:
             prefix_name = prefix.split("/")[-1]
-            logo_dict[prefix_name] = MolecularInformation.FunctionLogo(args.infernal, "infernal", args.nosingle)
+            logo_dict[prefix_name] = MolecularInformation.FunctionLogo(args.infernal, "infernal", None, args.nosingle)
     elif (args.single):
         for prefix in args.file_prefix:
             prefix_name = prefix.split("/")[-1]
-            logo_dict[prefix_name] = MolecularInformation.FunctionLogo(args.cove, "s", args.nosingle)
+            logo_dict[prefix_name] = MolecularInformation.FunctionLogo(None, "s", None, args.nosingle)
 
     for prefix in args.file_prefix:
         prefix_name = prefix.split("/")[-1]
